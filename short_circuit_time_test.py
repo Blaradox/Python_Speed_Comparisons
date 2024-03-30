bb378243fcfa696ca09567f66b6311fe4de2f182
Austin Sloane <sloane.ats@gmail.com>
2024-03-29 22:52:39 -0700

    add more variables for readability

--- short_circuit_time_test.py
+++ short_circuit_time_test.py
@@ -15,17 +15,21 @@ def is_palindrome_sc(s: str) -> bool:
         return s[0] == s[1] and is_palindrome_sc(s[1:-1])
 
 def main():
-    print('Check if "1234567890987654321" is a palindrome')
-    print('\twith if statements\t',
-        timeit.timeit(lambda: is_palindrome_if("1234567890987654321"), number=1_000_000))
-    print('\twith short circuits\t',
-        timeit.timeit(lambda: is_palindrome_sc("1234567890987654321"), number=1_000_000))
+    PAL_EVEN="1234567890987654321"
+    PAL_ODD="12345678900987654321"
+    REPEATS=10_000_000
 
-    print('Check if "12345678900987654321" is a palindrome')
+    print(f'Check if {PAL_EVEN!r} is a palindrome, measured in seconds')
     print('\twith if statements\t',
-        timeit.timeit(lambda: is_palindrome_if("12345678900987654321"), number=1_000_000))
+        timeit.timeit(lambda: is_palindrome_if(PAL_ODD), number=REPEATS))
     print('\twith short circuits\t',
-        timeit.timeit(lambda: is_palindrome_sc("12345678900987654321"), number=1_000_000))
+        timeit.timeit(lambda: is_palindrome_sc(PAL_ODD), number=REPEATS))
+
+    print(f'Check if {PAL_EVEN!r} is a palindrome')
+    print('\twith if statements\t',
+        timeit.timeit(lambda: is_palindrome_if(PAL_EVEN), number=REPEATS))
+    print('\twith short circuits\t',
+        timeit.timeit(lambda: is_palindrome_sc(PAL_EVEN), number=REPEATS))
 
 if __name__ == '__main__':
     main()
--- sum_from_0_to_n-1.py
+++ sum_from_0_to_n-1.py
@@ -4,12 +4,14 @@ import numpy
 
 """
 Sum the numbers from 0 to n-1 in different ways.
-Courtesy of @jamesmurphy-mc:
+Modified from @jamesmurphy-mc:
     https://github.com/mCodingLLC/VideosSampleCode/blob/master/videos/031_the_fastest_way_to_loop_in_python_an_unfortunate_truth/fastest_way_to_loop.py
 """
 
+LIMIT=10_000_000
 
-def while_loop(n=100_000_000):
+
+def while_loop(n: int=LIMIT) -> int:
     i = 0
     s = 0
     while i < n:
@@ -18,14 +20,14 @@ def while_loop(n=100_000_000):
     return s
 
 
-def for_loop(n=100_000_000):
+def for_loop(n: int=LIMIT) -> int:
     s = 0
     for i in range(n):
         s += i
     return s
 
 
-def for_loop_with_increment(n=100_000_000):
+def for_loop_with_increment(n: int=LIMIT) -> int:
     s = 0
     for i in range(n):
         s += i
@@ -33,7 +35,7 @@ def for_loop_with_increment(n=100_000_000):
     return s
 
 
-def for_loop_with_test(n=100_000_000):
+def for_loop_with_test(n: int=LIMIT) -> int:
     s = 0
     for i in range(n):
         if i < n: pass
@@ -41,21 +43,63 @@ def for_loop_with_test(n=100_000_000):
     return s
 
 
-def for_loop_with_increment_and_test(n=100_000_000):
+def for_loop_with_increment_and_test(n: int=LIMIT) -> int:
     s = 0
     for i in range(n):
         if i < n: pass
         i += 1
         s += i
     return s
+
+def sum_range(n: int=LIMIT) -> int:
+    return sum(range(n))
+
+
+def sum_generator(n: int=LIMIT) -> int:
+    return sum(i for i in range(n))
+
+
+def sum_list_comp(n: int=LIMIT) -> int:
+    return sum([i for i in range(n)])
+
+
+def sum_numpy(n: int=LIMIT) -> int:
+    return numpy.sum(numpy.arange(n, dtype=numpy.int64))
+
+
+def sum_numpy_python_range(n: int=LIMIT) -> int:
+    return numpy.sum(range(n))
+
+
+def sum_math(n: int=LIMIT) -> int:
+    return (n * (n - 1)) // 2
 
 
 def main():
-    print('while loop\t\t', timeit.timeit(while_loop, number=1))
-    print('for pure\t\t', timeit.timeit(for_loop, number=1))
-    # print('for inc\t\t\t', timeit.timeit(for_loop_with_increment, number=1))
-    # print('for test\t\t', timeit.timeit(for_loop_with_test, number=1))
-    # print('for inc+test\t', timeit.timeit(for_loop_with_increment_and_test, number=1))
+    print(f'Sum all numbers from 1 to {LIMIT:,}, measured in seconds')
+    print('\twhile loop\t\t\t\t',
+          timeit.timeit(while_loop, number=1))
+    print('\tfor loop\t\t\t\t',
+          timeit.timeit(for_loop, number=1))
+    print('\tfor loop with unnecessary increment\t',
+          timeit.timeit(for_loop_with_increment, number=1))
+    print('\tfor loop with unnecessary test\t\t',
+          timeit.timeit(for_loop_with_test, number=1))
+    print('\tfor loop with both inc+test\t\t',
+          timeit.timeit(for_loop_with_increment_and_test, number=1))
+    print('\tsum of range\t\t\t\t',
+          timeit.timeit(sum_range, number=1))
+    print('\tsum using unnecessary generator\t\t',
+          timeit.timeit(sum_generator, number=1))
+    print('\tsum using list comprehension\t\t',
+          timeit.timeit(sum_list_comp, number=1))
+    print('\tnumpy sum\t\t\t\t',
+          timeit.timeit(sum_numpy, number=1))
+    print('\tnumpy sum python range\t\t\t',
+          timeit.timeit(sum_numpy_python_range, number=1))
+    print('\tpure math sum\t\t\t\t',
+          timeit.timeit(sum_math, number=1))
+
 
 if __name__ == '__main__':
     main()
